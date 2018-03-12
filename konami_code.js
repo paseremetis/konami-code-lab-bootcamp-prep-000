b9a277066bff4ffc6021e0c1e1aacd4812ea88e0
Peter Seremetis <pswriter5348@gmail.com>
2018-03-12 20:02:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  for(let i = 0; i < code.length; i++){
-    if (e.which === code.length) {
-      alert (`Congratulations!`);
-    }else{
-      addEventListener
+  code.addEventListener('keydown', function(e) {
+    for(let i = 0; i < code.length; i++){
+      if (e.which === code.length) {
+        alert (`Congratulations!`);
+       }
+      }
     }
   }
+})
   // Write your JavaScript code inside the init() function
   //addEventListener()
   //if all keys, alert congratulations!
