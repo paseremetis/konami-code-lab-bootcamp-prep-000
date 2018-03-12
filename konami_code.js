95946849ccc670b62869ec2f5bceddbb3853bf10
Peter Seremetis <pswriter5348@gmail.com>
2018-03-12 19:57:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  for(let i = 0; i < code.length; i++){
+    if (e.which === code.length) {
+      alert (`Congratulations!`);
+    }else{
+      addEventListener
+    }
+  }
   // Write your JavaScript code inside the init() function
+  //addEventListener()
+  //if all keys, alert congratulations!
+  //else alert nothing
 
 }
